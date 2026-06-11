cc555699e2d07b745a3aa05f52014b053a517472
Jesse Bowes <647843+jessebs@users.noreply.github.com>
2026-06-10 23:03:27 -0700

    Better branch coverage calculations (#54)

--- lib/simplecov-cobertura.rb
+++ lib/simplecov-cobertura.rb
@@ -71,14 +71,21 @@ module SimpleCov
               class_.add_element(REXML::Element.new('methods'))
               class_.add_element(lines = REXML::Element.new('lines'))
 
-              branched_lines = file.branches.map(&:start_line)
-              branched_lines_covered = file.covered_branches.map(&:start_line)
+              branches_by_line = {}
+              if SimpleCov.branch_coverage?
+                file.branches.each do |branch|
+                  line_no = branch.start_line
+                  branches_by_line[line_no] ||= { total: 0, covered: 0 }
+                  branches_by_line[line_no][:total] += 1
+                  branches_by_line[line_no][:covered] += 1 if branch.covered?
+                end
+              end
 
               file.lines.each do |file_line|
                 if file_line.covered? || file_line.missed?
                   lines.add_element(line = REXML::Element.new('line'))
                   set_line_attributes(line, file_line)
-                  set_branch_attributes(line, file_line, branched_lines, branched_lines_covered) if SimpleCov.branch_coverage?
+                  set_branch_attributes(line, file_line, branches_by_line) if SimpleCov.branch_coverage?
                 end
               end
             end
@@ -135,11 +142,14 @@ module SimpleCov
           line.attributes['hits'] = file_line.coverage.to_s
         end
 
-        def set_branch_attributes(line, file_line, branched_lines, branched_lines_covered)
-          if branched_lines.include? file_line.number
-            pct_coverage, branches_covered = branched_lines_covered.include?(file_line.number) ? [100, '1/1'] : [0, '0/1']
+        def set_branch_attributes(line, file_line, branches_by_line)
+          branch_info = branches_by_line[file_line.number]
+          if branch_info
+            total = branch_info[:total]
+            covered = branch_info[:covered]
+            pct_coverage = total > 0 ? (covered * 100 / total) : 0
             line.attributes['branch'] = 'true'
-            line.attributes['condition-coverage'] = "#{pct_coverage}% (#{branches_covered})"
+            line.attributes['condition-coverage'] = "#{pct_coverage}% (#{covered}/#{total})"
           else
             line.attributes['branch'] = 'false'
           end
